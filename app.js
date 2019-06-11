5c50eeb85e1565bcf76126b40c4a5e379ae77fa1
Prabeen04 <prabeen.strange@gmail.com>
2019-06-11 09:24:03 +0530

    image layer added

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ var stage = new Konva.Stage({
 });
 
 // add canvas element
-var layer = new Konva.Layer();
+var layer = new Konva.Layer({
+    zIndex: 0
+});
+var layer2 = new Konva.Layer({
+    zIndex: 1
+});
 stage.add(layer);
 var group = new Konva.Group({
     x: 100,
@@ -32,12 +37,23 @@ var circle = new Konva.Circle({
     fill: '#00D2FF',
     stroke: 'black',
     strokeWidth: 4,
-    ShadowOffsetY: 10,
+    ShadowOffsetY: 14,
     ShadowOffsetX: 10,
-    ShadowBlur: 10,
+    ShadowBlur: 20,
     draggable: true
 });
+var imageObj = new Image();
 
+imageObj.onload = function () {
+    var image = new Konva.Image({
+        x: 200,
+        y: 50,
+        image: imageObj,
+        width: 100,
+        height: 100
+    });
+};
+imageObj.src = 'https://a10.gaanacdn.com/images/playlists/27/1038627/crop_175x175_1559652875_1038627.jpg'
 // circle.cache()
 // circle.filters([Konva.filter.Blur])
 // circle.blurRadius(10)
@@ -45,6 +61,8 @@ group.add(box)
 group.add(circle)
 layer.add(group);
 
+stage.add(layer2);
+layer2.add(imageObj)
 layer.draw();
 
 // add cursor styling
